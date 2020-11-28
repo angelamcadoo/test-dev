6e20b244d2d87c644259fcd527398a0d39ff7ff1
Angela McAdoo <angela.mcadoo@cloudchariot.tech>
2020-11-28 10:58:10 -0500

    Identity Risk Detection Pull Script

---
+++ Collect_Identity_Risk_Detections.ps1
@@ -0,0 +1,27 @@
+$ClientID   = "<Client ID here>" # This SHOULD be a 36 hex character string; insert your information here
+$ClientSecret   = "<your client secret here>"    # Should be a ~44 character string; insert your info here
+$tenantdomain   = "<your tenant domain here>"    # For example, cloudchariot.onmicrosoft.com
+
+$loginURL       = "https://portal.microsoft.com"
+$resource       = "https://graph.microsoft.com"
+
+$body       = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret}
+$oauth      = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+
+Write-Output $oauth
+
+if ($oauth.access_token -ne $null) {
+        $headerParams = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+
+        $url = "https://graph.microsoft.com/beta/identityRiskEvents"
+        Write-Output $url
+
+        $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $url)
+
+        foreach ($event in ($myReport.Content | ConvertFrom-Json).value) {
+            Write-Output $event
+        }
+
+} else {
+        Write-Host "ERROR: No Access Token"
+}
